8d7a024d77a7c7e0f36045ad0465aa7a668acbdb
nhatduyng <nguyennhatduy221099@gmail.com>
2022-11-01 21:20:17 +0700

    lesson02-data-structure 01.11.2022

---
+++ lesson02-data-structure/src/demo/Ex01ProgramStructure.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Ex01ProgramStructure {
+	public static void main(String[] args) {
+		System.out.println("Wellcome to class");
+		int value = sum(5,10);
+		System.out.println("Result " + value);
+	}
+	public static int sum(int a, int b) {
+		return a+b;
+	}
+}
