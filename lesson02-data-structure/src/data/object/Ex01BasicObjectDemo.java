36afd51d896ace452e0205926a830f2cdeae70e1
quyle <huuquyle9@gmail.com>
2022-11-01 21:20:17 +0700

    lesson02-data-structure 01.11.2022

---
+++ lesson02-data-structure/src/data/object/Ex01BasicObjectDemo.java
@@ -0,0 +1,16 @@
+package data.object;
+
+public class Ex01BasicObjectDemo {
+	public static void main(String[] args) {
+		int a=5;
+		Integer i1;
+		i1 =new Integer(15);
+		Double d1=new Double(25);
+		String s1 =new String("hello");
+		
+		System.out.println("i1: " + i1);
+		System.out.println("d1: " + d1);
+		System.out.println("s1: " + s1);
+	}
+
+}
