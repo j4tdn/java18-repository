0173a534118c94710f537bb83bdd96a52026f71d
HuuThai123 <phanhuuthai28072002@gmail.com>
2022-11-08 16:25:12 +0700

    lesson02-data-structure 06-11-2022

---
+++ lession02-data-structure/src/io/Ex01BasicIoDemo.java
@@ -0,0 +1,26 @@
+package io;
+
+import java.util.Scanner;
+
+public class Ex01BasicIoDemo {
+	public static void main(String[] args) {
+		// Enter firstName, lastName, email
+		Scanner ip = new Scanner(System.in);
+
+		System.out.print("Enter first name: ");
+		String firstName = ip.nextLine();
+
+		System.out.print("Enter last name: ");
+		String lastName = ip.nextLine();
+		
+		System.out.print("Enter age name: ");
+		//int age = ip.nextInt();
+		int age = Integer.parseInt(ip.nextLine());
+
+		System.out.print("Enter email name: ");
+		String email = ip.nextLine();
+		
+		System.out.println("Infor ==> " + firstName + " " + lastName + ", " + email);
+		System.out.println("Age :" + age);
+	}
+}
