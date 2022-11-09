062d7d5dafcd3d8c5f2bf0a6785bf68002b571fa
mnhat21 <minhnhat221201@gmail.com>
2022-11-10 03:39:37 +0700

    lesson02-mnhat21 08.11.2022

---
+++ lesson02-mnhat21/src/view/Ex01MultiplesOfTwo.java
@@ -0,0 +1,42 @@
+package view;
+
+import java.util.Scanner;
+
+public class Ex01MultiplesOfTwo {
+	public static void main(String[] args) {
+		Scanner ip = new Scanner(System.in);
+		int wrongTimes = -1;
+		String preCheck = "";
+		do {
+			wrongTimes++;
+			if (wrongTimes == 5) {
+				return;
+			}
+
+			System.out.print(wrongTimes > 0 ? "Please enter valid number (Wrong times -> " + wrongTimes + " ) : "
+					: "Enter a number to check : ");
+
+			preCheck = ip.nextLine();
+		} while (isNumber(preCheck) == false);
+
+		int n = Integer.parseInt(preCheck);
+		if (n % 2 == 0) {
+			System.out.println("=================== \nIs multiples of 2");
+		} else {
+			System.out.println("===================== \nIs not multiples of 2");
+		}
+	}
+
+	private static boolean isNumber(String input) {
+		if (input == "") {
+			return false;
+		}
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) < '0' || input.charAt(i) > '9') {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
