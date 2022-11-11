639f25833cc5c1615dbe4beb720e626650e676d3
tranthuyvan <tranthuyvan2105@gmail.com>
2022-11-11 10:26:23 +0700

    lesson02-tranthuyvan 11.11.2022

--- lesson02-tranthuyvan/src/view/Ex08Prime.java
+++ lesson02-tranthuyvan/src/view/Ex08Prime.java
@@ -69,7 +69,9 @@ public class Ex08Prime {
 		}
 
 		for (int i = 2; i < n - 1; i++) {
-			return false;
+			if (n % i == 0) {
+				return false;
+			}
 		}
 		return true;
 	}
--- lesson02-tranthuyvan/src/view/Ex09Prime200th.java
+++ lesson02-tranthuyvan/src/view/Ex09Prime200th.java
@@ -26,7 +26,9 @@ public class Ex09Prime200th {
 		}
 
 		for (int i = 2; i < n - 1; i++) {
-			return false;
+			if (n % i == 0) {
+				return false;
+			}
 		}
 		return true;
 	}
