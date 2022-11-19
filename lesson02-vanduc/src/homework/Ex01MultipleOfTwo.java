4e957e48c7bf73524f2458ad2bf1b1a8c5719a7d
vanduc262 <vanducsmile@gmail.com>
2022-11-20 00:02:00 +0700

    lesson-03 14.11.2022

--- lesson02-vanduc/src/homework/Ex01MultipleOfTwo.java
+++
@@ -1,7 +0,0 @@
-package homework;
-
-public class Ex01MultipleOfTwo {
-	public static void main(String[] args) {
-		
-	}
-}
--- lesson02-vanduc/src/homework/Ex09The200PrimeNumber.java
+++
@@ -1,7 +0,0 @@
-package homework;
-
-public class Ex09The200PrimeNumber {
-	public static void main(String[] args) {
-		
-	}
-}
