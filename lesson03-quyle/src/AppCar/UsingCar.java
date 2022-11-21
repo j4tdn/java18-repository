24b5094bb374d3b979e2f3c35ea994f8237bfe52
quyle <huuquyle9@gmail.com>
2022-11-21 15:53:00 +0700

    lesson03-quyle 21.11.2022

---
+++ lesson03-quyle/src/AppCar/UsingCar.java
@@ -0,0 +1,30 @@
+package AppCar;
+
+public class UsingCar {
+	private Car car;
+	private Customer customer;
+	public UsingCar() {
+	}
+	public UsingCar(Car car, Customer customer) {
+		this.car = car;
+		this.customer = customer;
+	}
+	public Car getCar() {
+		return car;
+	}
+	public void setCar(Car car) {
+		this.car = car;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public String toString() {
+		return "UsingCar [car=" + car + ", customer=" + customer + "]";
+	}
+	
+	
+	
+}
