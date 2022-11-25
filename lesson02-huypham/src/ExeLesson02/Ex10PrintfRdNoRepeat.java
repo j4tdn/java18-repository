71c7c1670d877caaa1272f9f3f4cf74ae83ed6b9
hpham <conghuypham98@gmail.com>
2022-11-25 10:13:08 +0700

    lesson02-huypham 25.11.2022

---
+++ lesson02-huypham/src/ExeLesson02/Ex10PrintfRdNoRepeat.java
@@ -0,0 +1,39 @@
+package ExeLesson02;
+
+import java.util.Random;
+
+public class Ex10PrintfRdNoRepeat {
+	public static void main(String[] args) {
+		int a =1, b=1, c=1, d=1, e=1;
+		
+		do {
+			Random rand = new Random();
+			a = rand.nextInt(11) + 20;
+			b = rand.nextInt(11) + 20;
+			c = rand.nextInt(11) + 20;
+			d = rand.nextInt(11) + 20;
+			e = rand.nextInt(11) + 20;
+		} while(repeat(a, b, c, d, e));
+		System.out.println("a : "+a);
+		System.out.println("b : "+b);
+		System.out.println("c : "+c);
+		System.out.println("d : "+d);
+		System.out.println("e : "+e);
+	}
+	
+	private static boolean repeat(int a, int b, int c, int d, int e) {
+		if (a == b || a == c || a == d || a == e) {
+			return true;
+		}
+		if (b == c || b == d || b == e) {
+			return true;
+		}
+		if (c == d || c == e) {
+			return true;
+		}
+		if (d == e) {
+			return true;
+		}
+		return false;
+	}
+}
