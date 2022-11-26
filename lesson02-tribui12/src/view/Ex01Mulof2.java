a9bbf57479f7a6e156679f7c70e3bd0897ceb20a
buiminhtri99 <buiminhtri99@gmail.com>
2022-11-26 09:40:31 +0700

    lesson02-trib12 26.11.2022

---
+++ lesson02-tribui12/src/view/Ex01Mulof2.java
@@ -0,0 +1,41 @@
+package view;
+
+import java.util.Scanner;
+public class Ex01Mulof2 {
+	public static void main(String[] args) {
+		Scanner ip = new Scanner(System.in);
+		int wrongTimes = -1;
+		String check = "";
+		do {
+			wrongTimes++;
+			if (wrongTimes == 5) {
+				return;
+			}
+
+			System.out.print(wrongTimes > 0 ? "enter valid number (Wrong times -> " + (wrongTimes) + " ) : "
+					: "Enter a number: ");
+
+			check = ip.nextLine();
+		} while (isNumber(check) == false);
+
+		int n = Integer.parseInt(check);
+		if (n % 2 == 0) {
+			System.out.println(" Multiples of 2");
+		} else {
+			System.out.println(" Not multiples of 2");
+		}
+	}
+
+	private static boolean isNumber(String input) {
+		if (input == "") {
+			return false;
+		}
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) < '0' || input.charAt(i) > '9') {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
