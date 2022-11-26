d557f106f9499177426b6828cdcb2f5f3733f361
HuuThai123 <phanhuuthai28072002@gmail.com>
2022-11-26 10:43:20 +0700

    lesson02-phanhuuthai 26.11.2022

---
+++ lesson02-phanhuuthai/src/exercises/Ex08PrimeNumber.java
@@ -0,0 +1,54 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Ex08PrimeNumber {
+	public static void main(String[] args) {
+		int wrongTimes = 0;
+		Scanner ip = new Scanner(System.in);
+		String preNum = "";
+
+		do {
+			String a = wrongTimes > 0 ? "(" + wrongTimes + "): " : ": ";
+			System.out.print("Enter your number" + a);
+			preNum = ip.nextLine();
+			if (isNumber(preNum)) {
+				break;
+			}
+			wrongTimes++;
+			if (wrongTimes == 5) {
+				System.out.println("wrongTimes = 5 ........... Exit.........");
+				System.exit(0);
+			}
+		} while (true);
+
+		int N = Integer.parseInt(preNum);
+		if (isPrime(N)) {
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+
+	}
+
+	private static boolean isPrime(int a) {
+		for (int i = 2; i <= (a / 2); i++) {
+			if (a % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean isNumber(String text) {
+		if (text == "") {
+			return false;
+		}
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) < '0' || text.charAt(i) > '9') {
+				return false;
+			}
+		}
+		return true;
+	}
+}
