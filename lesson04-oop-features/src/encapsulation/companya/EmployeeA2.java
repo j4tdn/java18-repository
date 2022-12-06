517605d36e467261531bf1ad2ee6df8224d98161
Quyen Phan <Quyen.Phan@mgm-tp.com>
2022-12-07 04:58:37 +0700

    lesson04-oop-features 06.12.2022

---
+++ lesson04-oop-features/src/encapsulation/companya/EmployeeA2.java
@@ -0,0 +1,14 @@
+package encapsulation.companya;
+
+public class EmployeeA2 {
+	public static void main(String[] args) {
+		System.out.println("A1 --> " + EmployeeA1.name);
+		System.out.println("A1 --> " + EmployeeA1.skills);
+		
+		// no access modifier
+		System.out.println("A1 --> " + EmployeeA1.assignedProjects);
+		
+		// private
+		// System.out.println("A1 --> " + EmployeeA1.salary);
+	}
+}
