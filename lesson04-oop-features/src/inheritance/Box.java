1d78b74204be8d3dde94135a4eb9331162b21c13
nhatduyng <nguyennhatduy221099@gmail.com>
2022-12-13 18:51:54 +0700

    learning

---
+++ lesson04-oop-features/src/inheritance/Box.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class Box extends Rectangle {
+	public static void main(String[] args) {
+		Box box = new Box();
+		box.paint();
+		box.clear();
+	}
+
+}
---
+++ lesson04-oop-features/src/inheritance/multiole/Mother.java
@@ -0,0 +1,11 @@
+package inheritance.multiole;
+
+public class Mother {
+	void codeDatabase() {
+		System.out.println("Mother ---> codeDatabase");
+	}
+	void codeFrontend() {
+		System.out.println("Father ----> JavaScript");
+	}
+
+}
