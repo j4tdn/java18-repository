49c31b1d62601c5ee5be2ca61cb20d7aebbf6988
nhatduyng <nguyennhatduy221099@gmail.com>
2022-12-13 18:51:54 +0700

    learning

---
+++ lesson04-oop-features/src/inheritance/multiole/Father.java
@@ -0,0 +1,10 @@
+package inheritance.multiole;
+
+public class Father {
+	void codeGit() {
+		System.out.println("Father ----> codeGit");
+	}
+	void codeFrontend() {
+		System.out.println("Father ----> HTML/CSS");
+	}
+}
