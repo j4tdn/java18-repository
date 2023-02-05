0606bda28962554b4d6ae71cfeb7cc4d7ec9d996
vancuong <ngovancuong1202@gmail.com>
2023-02-05 08:59:33 +0700

    Homework lesson07

---
+++ lesson07-vancuong01/src/Ex02/Homework.java
@@ -0,0 +1,28 @@
+package Ex02;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
+public class Homework {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter year: ");
+		int year = sc.nextInt();
+		System.out.print("Enter month: ");
+		int month = sc.nextInt();
+		System.out.print("Enter day: ");
+		int day = sc.nextInt();
+		LocalDate checkDate = LocalDate.of(year, month, day);
+		System.out.println(checkDate.getDayOfMonth() + " " + checkDate.getMonth() + " " + checkDate.getYear() + " is a "
+				+ checkDate.getDayOfWeek());
+		detailDay(checkDate);
+	}
+
+	public static void detailDay(LocalDate checkDate) {
+		long totalDay = ChronoUnit.DAYS.between(LocalDate.of(checkDate.getYear(), 1, 1),
+				LocalDate.of(checkDate.getYear(), 12, 31)) + 1;
+		System.out.println("It is day number " + checkDate.getDayOfYear() + " of the year, "
+				+ (totalDay - checkDate.getDayOfYear()) + " days left");
+	}
+}
