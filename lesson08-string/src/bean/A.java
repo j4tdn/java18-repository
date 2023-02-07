037dd0a6dc56f24544853cb2f8c0a5aad2b03555
tranthuyvan <tranthuyvan2105@gmail.com>
2023-02-07 20:50:22 +0700

    lesson08-string 07.02.2023

---
+++ lesson08-string/src/bean/A.java
@@ -0,0 +1,44 @@
+package bean;
+
+public class A {
+	private int p;
+
+	public A(int p) {
+		this.p = p;
+	}
+
+	public int getP() {
+		return p;
+	}
+
+	public void setP(int p) {
+		this.p = p;
+	}
+
+	// a1.equals(a2)
+	@Override
+	public boolean equals(Object o) {
+		// trùng địa chỉ
+		if (this == o) {
+			return true;
+		}
+		// khác địa chỉ
+		if (!(o instanceof A)) {
+			return false;
+		}
+
+		// o chắc chắn lúc runtime là A
+		A ax = this;
+		A ay = (A) o;
+
+		// 2 đối tượng ax và ay là equals nếu có giá trị của p bằng nhau
+		return ax.p == ay.p;
+
+	}
+
+	@Override
+	public String toString() {
+		return "A [p=" + p + "]";
+	}
+
+}
