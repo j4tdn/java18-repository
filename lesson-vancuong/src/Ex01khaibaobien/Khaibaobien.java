27d3275be9c5322742aefe8c94c8bdb8455a8916
vancuong <ngovancuong1202@gmail.com>
2023-02-08 19:19:53 +0700

    lesson-tuhoc

---
+++ lesson-vancuong/src/Ex01khaibaobien/Khaibaobien.java
@@ -0,0 +1,13 @@
+package Ex01khaibaobien;
+
+public class Khaibaobien {
+	public static void main(String[] args) {
+		String ho = "ngo";
+		String ten = "vancuong";
+		String hovaten = "ngovancuong";
+
+		System.out.println("ho : " + ho);
+		System.out.println("tên : " + ten);
+		System.out.println("h�? và tên: " + hovaten);
+	}
+}
---
+++ lesson-vancuong/src/Ex02hangso/Hangso.java
@@ -0,0 +1,8 @@
+package Ex02hangso;
+
+public class Hangso {
+	public static void main(String[] args) {
+		final int x = 100;
+		System.out.println("x =" + x);
+	}
+}
