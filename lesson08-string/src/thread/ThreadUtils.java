ec6b9a0cc528de38eb7b391376d955133ee6aa5a
tranthuyvan <tranthuyvan2105@gmail.com>
2023-02-14 16:41:50 +0700

    lesson08-String 14.02.2023

---
+++ lesson08-string/src/thread/ThreadUtils.java
@@ -0,0 +1,31 @@
+package thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadUtils {
+
+	private ThreadUtils() {
+	}
+
+	public static void sleep(long seconds) {
+		try {
+			TimeUnit.SECONDS.sleep(seconds);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void doTask(String taskName, long executionTimeInSeconds) {
+		try {
+			System.out
+					.println(">> " + taskName + " is being executed ... by thread " + Thread.currentThread().getName());
+			for (int i = 1; i <= executionTimeInSeconds; i++) {
+				System.out.println("Task " + taskName + " ... " + i);
+				TimeUnit.SECONDS.sleep(1);
+			}
+			System.out.println(">> Task " + taskName + " is finished ...");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
