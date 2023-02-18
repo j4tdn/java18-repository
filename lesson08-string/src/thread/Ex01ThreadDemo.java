b3a2f7cc85ced4d72e6f98b10321d1cb573609bd
Vuong Viet Tay <tayvuong46@gmail.com>
2023-02-19 00:05:14 +0700

    lesson08-string 19.02.2023

---
+++ lesson08-string/src/thread/Ex01ThreadDemo.java
@@ -0,0 +1,23 @@
+package thread;
+
+import static thread.ThreadUtils.*;
+
+public class Ex01ThreadDemo {
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		System.out.println("Start main method ...");
+		System.out.println("thread name ---> " + Thread.currentThread().getName());
+
+		// Tạo thread mới
+		// start thread t1 --> t1.start() --> t1.run() --> runnable.run()
+		Thread t1 = new Thread(new Task("M2", 4, start), "T1");
+		t1.start();
+
+		sleep(2);
+
+		doTask("M1", 3);
+		System.out.println("End main method ...");
+		System.out.println("duration --> " + (System.currentTimeMillis() - start) + " ms");
+	}
+
+}
