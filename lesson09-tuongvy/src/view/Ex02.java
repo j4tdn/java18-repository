6df03cf00ef2a5845577407b69655cca2bb36714
Tuong Vy <vothituongvy2906@gmail.com>
2023-02-21 18:21:18 +0700

    lesson09-tuongvy

--- lesson09-tuongvy/src/view/Ex02.java
+++ lesson09-tuongvy/src/view/Ex02.java
@@ -1,6 +1,6 @@
 package view;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ex02 {
@@ -11,21 +11,29 @@ public class Ex02 {
 		int m = n;
 		int a;
 		int count = 0;
-		Arrays arr;
-		String s = String.valueOf(n);
-		for (int i = 0; i < n; i++) {
-			while(n > 0) {
-				a = n%10;
-				n = n/10;
-				count += a*a;
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		while (n > 0) {
+			while (n > 0) {
+				a = n % 10;
+				n = n / 10;
+				count += a * a;
 			}
-		
 			n = count;
 			count = 0;
 			if (n == 1) {
 				System.out.println(m + " la so hanh phuc");
 				break;
 			}
+
+			for (Integer j : arr) {
+				if (j == n) {
+					System.out.println(m + " khong phai so hanh phuc.");
+					n = 0;
+					break;
+				}
+			}
+			arr.add(n);
+
 		}
 	}
 
