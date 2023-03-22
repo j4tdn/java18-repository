24cc47dae4a6026aff853a4b91548676b7560c0e
TraTo <minhti195@gmail.com>
2023-03-22 19:37:39 +0700

    lesson10-traluong 22.3.2023

--- lesson10-traluong/src/view/Ex02.java
+++ lesson10-traluong/src/view/Ex02.java
@@ -3,26 +3,25 @@ package view;
 import java.util.Arrays;
 
 public class Ex02 {
+	static int row = 4;
+	static int col = 5;
 	public static void main(String[] args) {
-		int row = 4;
-		int col = 5;
 		Integer[][] data = new Integer[row][col];
 		Integer[][] datas = {
 			{1, 2, 3, 4},
 			{5, 6, 7, 8},
 			{1, 9, 0, 9},
-			{2, 9, 6, 5},
-			{1, 2, 4, 5}
+			{2, 1, 6, 5},
+			{1, 2, 4, 0}
 		};
 		data = datas;
-		printMatrix(data, row, col);
-		Integer[] location = findLocationOfZero(data, row, col);
-		System.out.println(Arrays.toString(findLocationOfZero(data, row, col)));
-		data = matrixTransfer(data, row, col, location);
+		String[] locationOfZeros = findLocationsOfZero(data);
 		
-		printMatrix(data, row, col);
+		System.out.println(Arrays.toString(locationOfZeros));
+		
+		printMatrix(matrixTransfer(data, locationOfZeros));
 	}
-	public static void printMatrix (Integer[][] matrix, int row, int col) {
+	public static void printMatrix (Integer[][] matrix) {
 		for (int i = 0; i < col; i++) {
 			for (int j = 0; j < row; j++) {
 				System.out.print(matrix[i][j] + " ");
@@ -30,25 +29,43 @@ public class Ex02 {
 			System.out.println();
 		}
 	}
-	public static Integer[] findLocationOfZero(Integer[][] matrix, int row, int col) {
-		Integer[] location = new Integer[2];
-		// location[0] --> number of row
-		// location[1] --> number of col
+	public static int numberOfZero(Integer[][] matrix) {
+		int count = 0;
+		for (int i = 0; i < col; i++) {
+			for (int j = 0; j < row; j++) {
+				if (matrix[i][j] == 0) count++;
+		
+			}
+		}
+		return count;
+	}
+	public static String[] findLocationsOfZero(Integer[][] matrix) {
+		String[] location = new String[numberOfZero(matrix)];
+		// 	i --> number of row
+		//	j --> number of col
+		int k = 0;
 		for (int i = 0; i < col; i++) {
 			for (int j = 0; j < row; j++) {
 				if (matrix[i][j] == 0) {
-					location[0] = j; 
-					location[1] = i;
+					location[k] = j + ";" + i;
+					k++;
 				}
 			}
 		}
 		return location;
 	}
-	public static Integer[][] matrixTransfer(Integer[][] matrix, int row, int col, Integer[] location) { 
-		for (int i = 0; i < col; i++) {
-			for (int j = 0; j < row; j++) {
-				if (j == location[0] || i == location[1]) {
-					matrix[i][j] = 0;
+	public static Integer[][] matrixTransfer(Integer[][] matrix, String[] location) {
+		
+		for (String e: location) {
+			
+			int x = Integer.parseInt(String.valueOf(e.charAt(2)));
+			int y = Integer.parseInt(String.valueOf(e.charAt(0)));
+			
+			for (int i = 0; i < col; i++) {
+				for (int j = 0; j < row; j++) {
+					if (i == x || j == y) {
+						matrix[i][j] = 0;
+					}
 				}
 			}
 		}
