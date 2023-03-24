13bd51ee36a951017ef3a4176f6c4c57ea6221ec
ndhuy <dhuy221001@gmail.com>
2023-03-24 12:25:57 +0700

    update

--- lesson11_collection/src/collections/list/Ex07LinkedListBasicDemo.java
+++ lesson11_collection/src/collections/list/Ex07LinkedListBasicDemo.java
@@ -6,5 +6,9 @@ import java.util.List;
 public class Ex07LinkedListBasicDemo {
 	public static void main(String[] args) {
 		List<Double> averagePoints = new LinkedList<>();
+		averagePoints.add(7.8d);
+		averagePoints.add(8.2d);
+		
+		System.out.println("size --> " + averagePoints.size());
 	}
 }
