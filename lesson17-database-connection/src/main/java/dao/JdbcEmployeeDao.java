6c5e11a53c6b1eea7bd649131e34bc4b8d4994fb
Quyen Phan <Quyen.Phan@mgm-tp.com>
2023-08-30 21:09:00 +0700

    lesson17-database-connection 30.08.2023

---
+++ lesson17-database-connection/src/main/java/dao/JdbcEmployeeDao.java
@@ -0,0 +1,73 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import bean.Employee;
+import connection.DbConnection;
+import utils.SqlUtils;
+
+public class JdbcEmployeeDao implements EmployeeDao {
+	
+	private static final int DEFAULT_DEPARTMENT_ID = 1;
+	
+	private static final String Q_REGISTER_NEW_ACCOUNT = ""
+			+ "INSERT INTO employee(EMAIL, PASSWORD, DEPARTMENT_ID)\n"
+			+ "	      VALUES(?, ?, ?)";
+	
+	private static final String Q_GET_EMPLOYEE_BY_ID = ""
+			+ "SELECT *\n"
+			+ "  FROM employee\n"
+			+ " WHERE email = ? AND password = ?";
+
+	private Connection connection;
+	private PreparedStatement pst;
+	private ResultSet rs;
+	
+	public JdbcEmployeeDao() {
+		connection = DbConnection.getConnection();
+	}
+	
+	@Override
+	public boolean register(String email, String password) {
+		try {
+			pst = connection.prepareStatement(Q_REGISTER_NEW_ACCOUNT);
+			pst.setString(1, email);
+			pst.setString(2, SqlUtils.md5(password));
+			pst.setInt(3, DEFAULT_DEPARTMENT_ID);
+			return pst.executeUpdate() > 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			SqlUtils.close(pst);
+		}
+		return false;
+	}
+	
+	@Override
+	public Employee login(String email, String password) {
+		Employee result = null;
+		try {
+			
+			String encryptedPassword = SqlUtils.md5(password);
+			
+			pst = connection.prepareStatement(Q_GET_EMPLOYEE_BY_ID);
+			pst.setString(1, email);
+			pst.setString(2, encryptedPassword);
+			rs = pst.executeQuery();
+			if (rs.next()) {
+				int id = rs.getInt("ID");
+				result = new Employee(email, encryptedPassword);
+				result.setId(id);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			SqlUtils.close(rs, pst);
+		}
+		return result;
+	}
+
+}
--- lesson17-database-connection/src/main/java/view/Item_View.java
+++ lesson17-database-connection/src/main/java/view/Item_View.java
@@ -1,7 +1,6 @@
 package view;
 
 import java.time.LocalDate;
-import java.util.AbstractMap;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
