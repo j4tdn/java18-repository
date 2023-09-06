6ef7f8e44b7b5cd3f8ec48d32fbecb0c8558615c
Quyen Phan <Quyen.Phan@mgm-tp.com>
2023-09-06 22:35:05 +0700

    lesson18-jpa-hibernate 06.09.2023

---
+++ lesson18-jpa-hibernate/src/main/java/persistence/ItemGroup.java
@@ -0,0 +1,54 @@
+package persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "item_group")
+@NamedQueries(
+	@NamedQuery(name = ItemGroup.GET_ALL_ITEM_GROUPS, query = "FROM ItemGroup") // JPQL
+)
+public class ItemGroup {
+	
+	public static final String GET_ALL_ITEM_GROUPS = "GET_ALL_ITEM_GROUPS";
+	
+	@Id
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	public ItemGroup() {
+	}
+
+	public ItemGroup(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "ItemGroup [id=" + id + ", name=" + name + "]";
+	}
+}
