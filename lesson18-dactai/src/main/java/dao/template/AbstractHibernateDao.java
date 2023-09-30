48c787a5ec53625dbf09f672f27400ecf4b0bbce
Huỳnh Lê Đắc Tài <dactai2k2@gmail.com>
2023-09-30 19:11:57 +0700

    lesson18-dactai

---
+++ lesson18-dactai/src/main/java/dao/template/AbstractHibernateDao.java
@@ -0,0 +1,31 @@
+package dao.template;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import connection.DbConnection;
+
+public class AbstractHibernateDao {
+	private SessionFactory sessionFactory;
+
+	public AbstractHibernateDao() {
+		sessionFactory = DbConnection.getSessionFactory();
+	}
+
+	protected Session openSession() {
+		return sessionFactory.openSession();
+	}
+
+	protected Session getCurrentSession() {
+		return sessionFactory.getCurrentSession();
+	}
+
+	@SuppressWarnings("unchecked")
+	protected <T> List<T> safeList(Query<?> query) {
+		return (List<T>) query.getResultList();
+	}
+
+}
