bb0800cc779ac3f6e599d99b95924f45d5f720cd
TraTo <minhti195@gmail.com>
2023-12-16 09:35:27 +0700

    added bs5 lession

---
+++ 3.BOOTSTRAP5/js/script.js
@@ -0,0 +1,8 @@
+/*window.innerWidth --> current width of web browser*/
+window.onload = function() {
+	window.document.querySelector('.wpx').textContent = window.innerWidth + ' px';
+}
+
+window.onresize = function() {
+	window.document.querySelector('.wpx').textContent = window.innerWidth + ' px';
+}
